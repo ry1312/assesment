4fd652defafe19b59280e27c59216c27c987b2ea
ry1312 <99656006+ry1312@users.noreply.github.com>
2022-02-14 20:46:59 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+$(function(){
+$('.drawer-icon').click(function(){
+
+  $('.drawer-icon').toggleClass('is-active');
+  $('.drawer-content').toggleClass('is-active');
+  $('.drawer-background').toggleClass('is-active');
+
+  //calss . .....忘れてた
+});
+
+//背景クリックしたら消えるやつ
+$('.drawer-content-item').click(function(){
+  $('.drawer-icon').toggleClass('is-active');
+  $('.drawer-content').toggleClass('is-active');
+  $('.drawer-background').toggleClass('is-active');
+});
+$('.drawer-background').click(function(){
+  $('.drawer-icon').toggleClass('is-active');
+  $('.drawer-content').toggleClass('is-active');
+  $('.drawer-background').toggleClass('is-active');
+});
+
+});
